c72fbce55aec5e2541aff35adee0ad0daf413df3
Alex-coder50 <thehonoredone995@gmail.com>
2025-11-30 20:44:09 +0200

    Add files via upload

---
+++ lesson 25/main.js
@@ -0,0 +1,10 @@
+var a = 8;
+var b = 10;
+var c = 10;
+var d = null;
+var e; 
+
+document.write("a and b are equal: " + (a==b) + "<br>");
+document.write("b and c are equal: " + (b==c) + "<br>");
+document.write("d is : " + d + "<br>");
+document.write("e is : " + e + "<br>");
